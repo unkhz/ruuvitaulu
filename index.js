eca64b2a4b85b59c2d024153219a142db40ef6a0
Juhani Pelli <juhani.pelli@gmail.com>
2020-10-13 22:43:38 +0300

    Add prom-client and express with defaults

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const ruuvi = require('node-ruuvitag')
+const { Registry, collectDefaultMetrics } = require('prom-client')
+const express = require('express')
+const app = express()
+
+const config = { port: 3000 }
+
+const register = new Registry()
+collectDefaultMetrics({ register })
 
 ruuvi.on('found', (tag) => {
   console.log('Found RuuviTag, id: ' + tag.id)
@@ -14,4 +22,13 @@ ruuvi.on('found', (tag) => {
 
 ruuvi.on('warning', (message) => {
   console.error(new Error(message))
+})
+
+app.get('/metrics', function (req, res) {
+  res.status(200).set('Content-Type', 'text/plain')
+  res.end(register.metrics())
+})
+
+app.listen(config.port, () => {
+  console.log(`Ruuvi test app listening at http://localhost:${config.port}`)
 })
